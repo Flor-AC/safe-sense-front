90d3af7486410db6f38e841eafca7d95c585e162
Flor-AC <florac206@gmail.com>
2021-06-01 23:52:19 -0500

    se coloco timer en subir repo

--- src/app/Imagen/subir/subir.component.ts
+++ src/app/Imagen/subir/subir.component.ts
@@ -8,12 +8,23 @@ import {Router} from "@angular/router";
 })
 export class SubirComponent {
   mostraralert: boolean;
+  day: number;
+  month: number;
+  year: number;
+  hour: number;
+  minute: number;
 
   constructor(private router: Router) {
     this.mostraralert = false;
+    this.day = 0;
+    this.month = 0;
+    this.year = 0;
+    this. hour = 0;
+    this.minute = 0;
   }
 
   ngOnInit(): void {
+    this.obtenerFecha();
   }
 
   Mostrar() {
@@ -25,5 +36,17 @@ export class SubirComponent {
 
   Subir() {
     this.mostraralert = true;
+  }
+
+  obtenerFecha(){
+     var fecha = new Date();
+     this.day = fecha.getDate();
+     this.month = fecha.getMonth();
+     this.year = fecha.getFullYear();
+     this.hour = fecha.getHours();
+     this.minute = fecha.getMinutes();
+
+    var fechaActual = fecha.getTime();
+    console.log(fechaActual);
   }
 }
